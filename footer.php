bdfaf058d190aa9f0066281ee4031d6654647e0e
Phil Cable <pcable@wsu.edu>
2018-05-31 11:11:27 -0700

    Update header and footer design
    This removes the max width on these elements and adjusts the font
    smoothing to better match the mockup.
    
    It also moves a few rules to avoid having to fight against the
    cascade.

--- footer.php
+++ footer.php
@@ -2,23 +2,19 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 
-		<div class="wrap">
+		<nav class="footer-nav">
 
-			<nav class="footer-nav">
+			<?php
+			wp_nav_menu( array(
+				'theme_location' => 'footer',
+				'menu_class' => '',
+				'container' => '',
+				'fallback_cb' => false,
+				'depth' => 1,
+			) );
+			?>
 
-				<?php
-				wp_nav_menu( array(
-					'theme_location' => 'footer',
-					'menu_class' => '',
-					'container' => '',
-					'fallback_cb' => false,
-					'depth' => 1,
-				) );
-				?>
-
-			</nav>
-
-		</div>
+		</nav>
 
 	</footer>
 
